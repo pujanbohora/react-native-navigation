1bc2a4d4564e05f6f69a66c7069e1a65aee440b5
Pujan Bohora <bohorapawan@gmail.com>
2023-07-10 15:52:45 +0545

    test

--- Navigation.js
+++ Navigation.js
@@ -9,12 +9,12 @@ const Tab = createBottomTabNavigator();
 
 function TabGroup(){
     return(
-        <Tab.Navigator>
+        <Tab.Navigatorssss>
             <Tab.Screen name='Feed' component={Feed}  />
             <Tab.Screen name='Notification' component={Notifications} />
             <Tab.Screen name='Settings' component={Settings} />
             <Tab.Screen name='Settings' component={Settings} />
-        </Tab.Navigator>
+        </Tab.Navigatorssss>
     )
 }
 
